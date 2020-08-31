c240c2b2e7479fcbef1de1c091e26587c6f82b5d
SarunSeepomkun <63921535+SarunSeepomkun@users.noreply.github.com>
2020-08-31 23:24:19 +0700

    test build

---
+++ build/precache-manifest.914d29b4f4abd341e61302be31442cf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1a5766d038cb13e71069a280af9cd35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef5f8f7ab9bbeff20312",
+    "url": "/static/css/main.020b475b.chunk.css"
+  },
+  {
+    "revision": "270b89a193ab9a043630",
+    "url": "/static/js/2.2ea97c68.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2ea97c68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef5f8f7ab9bbeff20312",
+    "url": "/static/js/main.18b559bd.chunk.js"
+  },
+  {
+    "revision": "d1c2a804f3991e8857e3",
+    "url": "/static/js/runtime-main.430a1900.js"
+  }
+]);
