eaa58df007198ca93e4f1dcb443d8f33ea2dc83d
SarunSeepomkun <63921535+SarunSeepomkun@users.noreply.github.com>
2020-09-04 23:39:17 +0700

    add picture

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import React from "react";
 import "./App.css";
 import MyTimeline from "./components/MyTimeline";
 import Personal from "./components/Personal";
-import BottomMenu from "./components/BottomMenu";
+// import BottomMenu from "./components/BottomMenu";
 import HeaderMenu from "./components/HeaderMenu";
 import Box from "@material-ui/core/Box";
 // import { positions } from "@material-ui/system";
 import { makeStyles } from "@material-ui/core/styles";
-import Skills from "./components/Skills";
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
-  root: { width:"100%", maxWidth: "850px" },
+  root: { width: "100%", maxWidth: "850px" },
 });
 
 function App() {
@@ -35,28 +31,10 @@ function App() {
           <HeaderMenu />
         </Box>
         {/* content */}
-        <Box
-          className={classes.root}
-        >
-          <Card className={classes.root}>
-            <CardContent>
-              <Typography variant="h5" component="h2">
-                <Box m={2}>
-                  <Personal />
-                </Box>
-              </Typography>
-              <Typography variant="body2" component="p">
-                <Box
-                  m={2}
-                  display="flex"
-                  justifyContent="center"
-                  flexWrap="wrap"
-                >
-                  <Skills />
-                </Box>
-              </Typography>
-            </CardContent>
-          </Card>
+        <Box className={classes.root}>
+          <Box m={2}>
+            <Personal />
+          </Box>
 
           <Box m={2}>
             <MyTimeline />
@@ -64,7 +42,7 @@ function App() {
         </Box>
         {/* footer */}
         <Box style={{ width: "100%" }}>
-          <BottomMenu />
+          {/* <BottomMenu /> */}
         </Box>
       </Box>
     </div>
--- src/components/Personal.js
+++ src/components/Personal.js
@@ -1,22 +1,65 @@
-import React from 'react';
-import Container from "@material-ui/core/Container";
-import CssBaseline from '@material-ui/core/CssBaseline';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Ipeach from "../images/sarun_seepomkun.jpeg";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 
-function Personal(){
-    return(
-        <React.Fragment>
-        <CssBaseline />
-        <Container>
-          <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '50vh' }} >
-          SARUN SEEPOMKUN
-Software Developer
- 095-920-6644
- Sarunseepomkun@gmail.com
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    minHeight: "200px",
+  },
+  details: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  content: {
+    flex: "1 0 auto",
+  },
+  cover: {
+    width: 250,
+    height: 190,
+    margin: "10px",
+  },
+  controls: {
+    display: "flex",
+    // alignItems: "center",
+    // paddingLeft: theme.spacing(1),
+    // paddingBottom: theme.spacing(1),
+  },
+}));
+
+function Personal() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardMedia
+        className={classes.cover}
+        image={Ipeach}
+        title="sarun_seepomkun"
+      />
+      <div className={classes.details}>
+        <CardContent className={classes.content}>
+          <Typography component="h5" variant="h5">
+            Mr.Sarun Seepomkun
           </Typography>
-        </Container>
-      </React.Fragment>
-    )
+          {/* <Typography variant="subtitle1" color="textSecondary">
+            Software developer
+          </Typography> */}
+          <div className={classes.controls}>
+            <Typography subtitle1="h2">
+              Software developer for 8 years participating in the complete
+              product development lifecycle of successfully launched
+              applications.Enthusiastic new programming technology
+            </Typography>
+          </div>
+        </CardContent>
+      </div>
+    </Card>
+  );
 }
 
 export default Personal;
