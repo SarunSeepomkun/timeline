be38671fb17525c9f2b9162bdbe2cef37e06c0ee
SarunSeepomkun <63921535+SarunSeepomkun@users.noreply.github.com>
2020-09-15 00:24:11 +0700

    change personal

--- src/components/BottomMenu.js
+++ src/components/BottomMenu.js
@@ -7,13 +7,7 @@ import CodeIcon from "@material-ui/icons/Code";
 import WorkIcon from "@material-ui/icons/Work";
 import RoomIcon from '@material-ui/icons/Room';
 
-const useStyles = makeStyles({
-  root: { maxWidth: 375 },
-  // stickyToBottom : { position: "sticky", width: "100%", bottom: "0" },
-});
-
 function BottomMenu() {
-  const classes = useStyles();
   const [value, setValue] = useState(0);
 
   return (
@@ -23,7 +17,6 @@ function BottomMenu() {
           setValue(newValue);
         }}
         showLabels={true}
-        className={classes.root}
       >
         <BottomNavigationAction label="Me" icon={<PermIdentityIcon />} />
         <BottomNavigationAction label="Exprerience" icon={<WorkIcon />} />
--- src/components/Personal.js
+++ src/components/Personal.js
@@ -1,65 +1,30 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import Ipeach from "../images/sarun_seepomkun.jpeg";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Typography from "@material-ui/core/Typography";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    minHeight: "200px",
-  },
-  details: {
-    display: "flex",
-    flexDirection: "column",
-  },
-  content: {
-    flex: "1 0 auto",
-  },
-  cover: {
-    width: 250,
-    height: 190,
-    margin: "10px",
-  },
-  controls: {
-    display: "flex",
-    // alignItems: "center",
-    // paddingLeft: theme.spacing(1),
-    // paddingBottom: theme.spacing(1),
-  },
-}));
+import './Personal.css';
 
 function Personal() {
-  const classes = useStyles();
-
   return (
-    <Card className={classes.root}>
-      <CardMedia
-        className={classes.cover}
-        image={Ipeach}
-        title="sarun_seepomkun"
+    <div className="container">
+      <img
+        className="imgProfile"
+        src={Ipeach}
+        alt="sarun_seepomkun"
       />
-      <div className={classes.details}>
-        <CardContent className={classes.content}>
-          <Typography component="h5" variant="h5">
+      <div>
+        <div>
+          <h1>
             Mr.Sarun Seepomkun
-          </Typography>
-          {/* <Typography variant="subtitle1" color="textSecondary">
-            Software developer
-          </Typography> */}
-          <div className={classes.controls}>
-            <Typography subtitle1="h2">
+          </h1>
+          <div>
+            <h2>
               Software developer for 8 years participating in the complete
               product development lifecycle of successfully launched
               applications.Enthusiastic new programming technology
-            </Typography>
+            </h2>
           </div>
-        </CardContent>
+        </div>
       </div>
-
-    </Card>
+    </div>
   );
 }
 
