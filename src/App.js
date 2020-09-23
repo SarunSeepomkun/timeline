9d21fabc23fb1050395d7120727744a2a3fa7973
SarunSeepomkun <63921535+SarunSeepomkun@users.noreply.github.com>
2020-09-23 22:56:13 +0700

    update design

--- src/App.js
+++ src/App.js
@@ -5,53 +5,29 @@ import Personal from "./components/Personal";
 import Skills from "./components/Skills";
 // import BottomMenu from "./components/BottomMenu";
 import HeaderMenu from "./components/HeaderMenu";
-import Box from "@material-ui/core/Box";
 // import { positions } from "@material-ui/system";
-import { makeStyles } from "@material-ui/core/styles";
 import Address from './components/Address';
 
-const useStyles = makeStyles({
-  root: { width: "100%", maxWidth: "850px" },
-});
-
 function App() {
-  const classes = useStyles();
-
   return (
-    <div style={{ width: "100%" }}>
-      <Box
-        display="flex"
-        flexDirection="row"
-        flexWrap="wrap"
-        justifyContent="center"
-        alignItems="flex-start"
-        alignContent="flex-start" 
-        minHeight="100%"
-      >
+    <div className="app__container">
         {/* header */}
-        <Box style={{ width: "100%" }}>
-          <HeaderMenu />
-        </Box>
+
+          {/* <HeaderMenu /> */}
+
         {/* content */}
-        <Box className={classes.root}>
-          <Box m={2}>
+        <div className="app_content">
             <Personal />
-          </Box>
-          <Box m={2} display="flex" justifyContent="center">
+
             <Skills />
-          </Box>
-          <Box m={2}>
+
             <MyTimeline />
-          </Box>
-          <Box m={2}>
+
           <Address />
-          </Box>
-        </Box>
+          </div>
         {/* footer */}
-        <Box style={{ width: "100%" }}>
+
           {/* <BottomMenu /> */}
-        </Box>
-      </Box>
     </div>
   );
 }
